f483accf1060c47ba3ada731927e8fc47273d914
katbow <katherine.bowler@gmail.com>
2017-11-12 13:27:51 +0000

    Create get_nth_prime_list function
    Returns initial list to be used for generating primes. Steps are tracked in generate_primes_list

--- lib/primes.ex
+++ lib/primes.ex
@@ -3,16 +3,18 @@ defmodule Primes do
   Documentation for Primes.
   """
 
+  def generate_primes_list(n) do
+    n
+    |> get_nth_prime_list()
+  end
+
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Primes.hello
-      :world
-
+  Creates a list of primes from 2,
+  which increments up to or greater than nth prime.
   """
-  def hello do
-    :world
+
+  def get_nth_prime_list(n) do
+    last_number = n * :math.log2(n) |> Float.ceil() |> trunc()
+    Enum.to_list(2..last_number)
   end
 end
