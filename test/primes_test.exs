64232bb4799d6a43ece825e2efb302100ed0f7d5
katbow <katherine.bowler@gmail.com>
2017-11-12 17:17:21 +0000

    Test all rows are included when making rows
    Required previous 2 tests to be slightly rewritten to get relevant row from list

--- test/primes_test.exs
+++ test/primes_test.exs
@@ -42,14 +42,21 @@ defmodule PrimesTest do
   end
 
   test "make_rows creates first row in correct format" do
-    actual = Primes.make_rows([2, 3, 5, 7], 1, 4)
+    actual = Primes.make_rows([2, 3, 5, 7], 4, [])
     expected = "|  |2 |3 |5 |7 |\n"
-    assert actual == expected
+    assert List.first(actual) == expected
   end
 
   test "make_rows creates row containing prime number + multiples up to last in list" do
-    actual = Primes.make_rows([2, 3, 5, 7], 2, 4)
+    actual = Primes.make_rows([2, 3, 5, 7], 4, [])
     expected = "|2 |4 |6 |10 |14 |\n"
+    assert Enum.at(actual, 1) == expected
+  end
+
+  test "make_rows gives list of rows" do
+    actual = Primes.make_rows([2, 3, 5, 7], 4, [])
+    expected = ["|  |2 |3 |5 |7 |\n", "|2 |4 |6 |10 |14 |\n",
+    "|3 |6 |9 |15 |21 |\n", "|5 |10 |15 |25 |35 |\n", "|7 |14 |21 |35 |49 |\n"]
     assert actual == expected
   end
 end
