80526f046e18ff29d92438d595d1de73b39e23b9
katbow <katherine.bowler@gmail.com>
2017-11-15 12:34:04 +0000

    Move tests for creating the table to table_test.exs Table functionality should be separate from primes functionality, so it should be moved to a separate module.

--- test/primes_test.exs
+++ test/primes_test.exs
@@ -1,8 +1,6 @@
 defmodule PrimesTest do
   use ExUnit.Case
 
-  import ExUnit.CaptureIO
-
   test "get_nth_prime_list creates list incrementing by 1" do
     assert Primes.get_nth_prime_list(4) == [2, 3, 4, 5, 6, 7, 8]
     assert Primes.get_nth_prime_list(3) == [2, 3, 4, 5]
@@ -43,34 +41,9 @@ defmodule PrimesTest do
     assert List.last(actual2) == 7919
   end
 
-  test "generate_primes_list gives list of n prime numbers for large n" do
-    actual = Primes.generate_primes_list(4000)
-    assert Kernel.length(actual) == 4000
-    assert List.last(actual) == 37813
-  end
-
-  test "make_rows creates first row in correct format" do
-    actual = Primes.make_rows([2, 3, 5, 7], 4, [])
-    expected = "|  |2 |3 |5 |7 |\n"
-    assert List.first(actual) == expected
-  end
-
-  test "make_rows creates row containing prime number + multiples up to last in list" do
-    actual = Primes.make_rows([2, 3, 5, 7], 4, [])
-    expected = "|2 |4 |6 |10 |14 |\n"
-    assert Enum.at(actual, 1) == expected
-  end
-
-  test "make_rows gives list of rows" do
-    actual = Primes.make_rows([2, 3, 5, 7], 4, [])
-    expected = ["|  |2 |3 |5 |7 |\n", "|2 |4 |6 |10 |14 |\n",
-    "|3 |6 |9 |15 |21 |\n", "|5 |10 |15 |25 |35 |\n", "|7 |14 |21 |35 |49 |\n"]
-    assert actual == expected
-  end
-
-  test "make_primes_mult_table generates string with all rows" do
-    actual = fn -> Primes.make_primes_mult_table(4) end
-    expected = "|  |2 |3 |5 |7 |\n|2 |4 |6 |10 |14 |\n|3 |6 |9 |15 |21 |\n|5 |10 |15 |25 |35 |\n|7 |14 |21 |35 |49 |\n\n"
-    assert capture_io(actual) == expected
-  end
+  # test "generate_primes_list gives list of n prime numbers for large n" do
+  #   actual = Primes.generate_primes_list(4000)
+  #   assert Kernel.length(actual) == 4000
+  #   assert List.last(actual) == 37813
+  # end
 end
