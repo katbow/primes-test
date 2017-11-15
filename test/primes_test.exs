415008c70c0af0c8298dc874a535da19b9117977
katbow <katherine.bowler@gmail.com>
2017-11-15 13:00:54 +0000

    Update test to use last number from n rather than n Because last value is now piped through before get_nth_prime_list, it need to use this new value instead of n.

--- test/primes_test.exs
+++ test/primes_test.exs
@@ -2,12 +2,12 @@ defmodule PrimesTest do
   use ExUnit.Case
 
   test "get_nth_prime_list creates list incrementing by 1" do
-    assert Primes.get_nth_prime_list(4) == [2, 3, 4, 5, 6, 7, 8]
-    assert Primes.get_nth_prime_list(3) == [2, 3, 4, 5]
+    assert Primes.get_nth_prime_list(8) == [2, 3, 4, 5, 6, 7, 8]
+    assert Primes.get_nth_prime_list(5) == [2, 3, 4, 5]
   end
 
   test "get_nth_prime_list includes nth prime" do
-    actual = Primes.get_nth_prime_list(168)
+    actual = Primes.get_nth_prime_list(1242)
     expected_length = 1241
     nth_prime = 997
     assert Kernel.length(actual) == expected_length
@@ -15,8 +15,8 @@ defmodule PrimesTest do
   end
 
   test "get_nth_prime_list includes nth prime for n < 3" do
-    assert Primes.get_nth_prime_list(1) == [2]
-    assert Primes.get_nth_prime_list(2) == [2, 3]
+    assert Primes.get_nth_prime_list(2) == [2]
+    assert Primes.get_nth_prime_list(3) == [2, 3]
   end
 
   test "remove_multiples removes multiples of first 4 primes" do
@@ -41,9 +41,9 @@ defmodule PrimesTest do
     assert List.last(actual2) == 7919
   end
 
-  # test "generate_primes_list gives list of n prime numbers for large n" do
-  #   actual = Primes.generate_primes_list(4000)
-  #   assert Kernel.length(actual) == 4000
-  #   assert List.last(actual) == 37813
-  # end
+  test "generate_primes_list gives list of n prime numbers for large n" do
+    actual = Primes.generate_primes_list(4000)
+    assert Kernel.length(actual) == 4000
+    assert List.last(actual) == 37813
+  end
 end
