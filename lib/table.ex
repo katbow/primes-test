3d8a8eca76d324593f1b690401b3de3449c6935a
katbow <katherine.bowler@gmail.com>
2017-11-23 13:05:32 +0000

    Create helper function to determine operation to use for table data Depending on the table_data option passed in, the helper function will call make_primes_mult_table with a different list creation function

--- lib/table.ex
+++ lib/table.ex
@@ -2,6 +2,16 @@ defmodule Table do
   def entry_point(n, opts \\ [table_data: "primes"]) do
     helper(n, opts[:table_data])
   end
+
+  # can use 2 opts, one for table data, and one for table_operation to determine in helper
+  defp helper(n, table_data) do
+    case table_data do
+      "primes" ->
+        make_primes_mult_table(n, &Primes.generate_primes_list/1, &row_mult/2)
+      _ ->
+        make_primes_mult_table(n, &make_list/1, &row_sum/2)
+    end
+  end
     rows =
       n
       |> Primes.generate_primes_list()
